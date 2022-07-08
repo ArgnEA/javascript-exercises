aaed24d4fdcc5173ae1e88aef848734107de5f2e
Aragon Emanuel <aragon3m4@gmail.com>
2022-07-08 17:00:08 -0300

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    newArray = [];
+    array = args[0];
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            newArray.push(array[i]);
+        } 
+    }
+    return newArray;
 };
 
 // Do not edit below this line
