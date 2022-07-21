fd84b8dfde89a4d4347d7c34a6a7a64e3dfeb283
Aragon Emanuel <aragon3m4@gmail.com>
2022-07-21 17:14:58 -0300

    Finish 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+    let bookNames = [];
+    for (let i = 0; i < array.length; i++) {
+        bookNames[i] = array[i].title;
+    }
+    return bookNames;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
