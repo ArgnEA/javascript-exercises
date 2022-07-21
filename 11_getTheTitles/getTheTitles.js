556dfcabdb5be440732e32a693ecba25b966a47d
Aragon Emanuel <aragon3m4@gmail.com>
2022-07-21 17:19:05 -0300

    Change 11_getTheTitles to use the map method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,5 @@
 const getTheTitles = function(array) {
-    let bookNames = [];
-    for (let i = 0; i < array.length; i++) {
-        bookNames[i] = array[i].title;
-    }
-    return bookNames;
+    return array.map(book => book.title);
 }
 
 // Do not edit below this line
